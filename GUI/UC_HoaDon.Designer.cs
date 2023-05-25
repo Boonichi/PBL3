3d27a8c2a6b09d10011caed5ad15c418bc8ae87c
Boonichi <pvhung1302@gmail.com>
2023-05-26 05:14:29 +0700

    Release code

---
+++ GUI/UC_HoaDon.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Do_An
+{
+    partial class UC_HoaDon
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Detail = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Edit = new System.Windows.Forms.Button();
+            this.tb_IdNhanVien = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_SoDienThoai = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dtp_NgayTao = new System.Windows.Forms.DateTimePicker();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.tb_KhachHang = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_AddThongTinBaoHanh = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Detail
+            // 
+            this.btn_Detail.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btn_Detail.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btn_Detail.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_Detail.FlatAppearance.BorderSize = 0;
+            this.btn_Detail.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Detail.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold);
+            this.btn_Detail.Location = new System.Drawing.Point(1124, 427);
+            this.btn_Detail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Detail.Name = "btn_Detail";
+            this.btn_Detail.Size = new System.Drawing.Size(148, 57);
+            this.btn_Detail.TabIndex = 124;
+            this.btn_Detail.Text = "Xem chi tiết";
+            this.btn_Detail.UseVisualStyleBackColor = false;
+            this.btn_Detail.Click += new System.EventHandler(this.btn_Detail_Click);
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btn_Delete.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btn_Delete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_Delete.FlatAppearance.BorderSize = 0;
+            this.btn_Delete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Delete.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold);
+            this.btn_Delete.Location = new System.Drawing.Point(1124, 345);
+            this.btn_Delete.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(148, 57);
+            this.btn_Delete.TabIndex = 123;
+            this.btn_Delete.Text = "Xoá hoá đơn";
+            this.btn_Delete.UseVisualStyleBackColor = false;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // btn_Edit
+            // 
+            this.btn_Edit.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btn_Edit.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btn_Edit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_Edit.FlatAppearance.BorderSize = 0;
+            this.btn_Edit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Edit.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold);
+            this.btn_Edit.Location = new System.Drawing.Point(1124, 267);
+            this.btn_Edit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Edit.Name = "btn_Edit";
+            this.btn_Edit.Size = new System.Drawing.Size(148, 57);
+            this.btn_Edit.TabIndex = 122;
+            this.btn_Edit.Text = "Chỉnh sửa hoá đơn";
+            this.btn_Edit.UseVisualStyleBackColor = false;
+            this.btn_Edit.Click += new System.EventHandler(this.btn_Edit_Click);
+            // 
+            // tb_IdNhanVien
+            // 
+            this.tb_IdNhanVien.Font = new System.Drawing.Font("Calibri", 12.8F, System.Drawing.FontStyle.Bold);
+            this.tb_IdNhanVien.Location = new System.Drawing.Point(205, 20);
+            this.tb_IdNhanVien.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tb_IdNhanVien.Name = "tb_IdNhanVien";
+            this.tb_IdNhanVien.Size = new System.Drawing.Size(289, 34);
+            this.tb_IdNhanVien.TabIndex = 87;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 12.8F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(712, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 27);
+            this.label5.TabIndex = 80;
+            this.label5.Text = "SĐT";
+            // 
+            // tb_SoDienThoai
+            // 
+            this.tb_SoDienThoai.Font = new System.Drawing.Font("Calibri", 12.8F, System.Drawing.FontStyle.Bold);
+            this.tb_SoDienThoai.Location = new System.Drawing.Point(784, 62);
+            this.tb_SoDienThoai.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tb_SoDienThoai.Name = "tb_SoDienThoai";
+            this.tb_SoDienThoai.Size = new System.Drawing.Size(289, 34);
+            this.tb_SoDienThoai.TabIndex = 86;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dtp_NgayTao);
+            this.panel2.Controls.Add(this.btn_Save);
+            this.panel2.Controls.Add(this.tb_IdNhanVien);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.tb_SoDienThoai);
+            this.panel2.Controls.Add(this.label14);
+            this.panel2.Controls.Add(this.label15);
+            this.panel2.Controls.Add(this.label13);
+            this.panel2.Controls.Add(this.tb_KhachHang);
+            this.panel2.Location = new System.Drawing.Point(32, 60);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1203, 201);
+            this.panel2.TabIndex = 120;
+            // 
+            // dtp_NgayTao
+            // 
+            this.dtp_NgayTao.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtp_NgayTao.Location = new System.Drawing.Point(205, 70);
+            this.dtp_NgayTao.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.dtp_NgayTao.MaxDate = new System.DateTime(2999, 12, 31, 0, 0, 0, 0);
+            this.dtp_NgayTao.Name = "dtp_NgayTao";
+            this.dtp_NgayTao.Size = new System.Drawing.Size(289, 26);
+            this.dtp_NgayTao.TabIndex = 141;
+            this.dtp_NgayTao.Value = new System.DateTime(2022, 4, 22, 0, 0, 0, 0);
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btn_Save.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btn_Save.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_Save.FlatAppearance.BorderSize = 0;
+            this.btn_Save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Save.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold);
+            this.btn_Save.Location = new System.Drawing.Point(523, 150);
+            this.btn_Save.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(148, 37);
+            this.btn_Save.TabIndex = 134;
+            this.btn_Save.Text = "Lưu";
+            this.btn_Save.UseVisualStyleBackColor = false;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // label14
+            // 
+            this.label14.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Calibri", 12.8F, System.Drawing.FontStyle.Bold);
+            this.label14.Location = new System.Drawing.Point(645, 20);
+            this.label14.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(118, 27);
+            this.label14.TabIndex = 114;
+            this.label14.Text = "Khách hàng";
+            // 
+            // label15
+            // 
+            this.label15.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Calibri", 12.8F, System.Drawing.FontStyle.Bold);
+            this.label15.Location = new System.Drawing.Point(57, 20);
+            this.label15.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(127, 27);
+            this.label15.TabIndex = 113;
+            this.label15.Text = "ID nhân viên";
+            // 
+            // label13
+            // 
+            this.label13.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Calibri", 12.8F, System.Drawing.FontStyle.Bold);
+            this.label13.Location = new System.Drawing.Point(92, 70);
+            this.label13.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(93, 27);
+            this.label13.TabIndex = 116;
+            this.label13.Text = "Ngày tạo";
+            // 
+            // tb_KhachHang
+            // 
+            this.tb_KhachHang.Font = new System.Drawing.Font("Calibri", 12.8F, System.Drawing.FontStyle.Bold);
+            this.tb_KhachHang.Location = new System.Drawing.Point(784, 18);
+            this.tb_KhachHang.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tb_KhachHang.Name = "tb_KhachHang";
+            this.tb_KhachHang.Size = new System.Drawing.Size(289, 34);
+            this.tb_KhachHang.TabIndex = 117;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 267);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1051, 420);
+            this.dataGridView1.TabIndex = 121;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 20F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(580, 14);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(246, 41);
+            this.label3.TabIndex = 119;
+            this.label3.Text = "Quản lí hoá đơn";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_AddThongTinBaoHanh
+            // 
+            this.btn_AddThongTinBaoHanh.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btn_AddThongTinBaoHanh.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btn_AddThongTinBaoHanh.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_AddThongTinBaoHanh.FlatAppearance.BorderSize = 0;
+            this.btn_AddThongTinBaoHanh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_AddThongTinBaoHanh.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold);
+            this.btn_AddThongTinBaoHanh.Location = new System.Drawing.Point(1124, 510);
+            this.btn_AddThongTinBaoHanh.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_AddThongTinBaoHanh.Name = "btn_AddThongTinBaoHanh";
+            this.btn_AddThongTinBaoHanh.Size = new System.Drawing.Size(148, 57);
+            this.btn_AddThongTinBaoHanh.TabIndex = 125;
+            this.btn_AddThongTinBaoHanh.Text = "Thêm thông tin bảo hành";
+            this.btn_AddThongTinBaoHanh.UseVisualStyleBackColor = false;
+            this.btn_AddThongTinBaoHanh.Click += new System.EventHandler(this.btn_AddThongTinBaoHanh_Click);
+            // 
+            // UC_HoaDon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btn_AddThongTinBaoHanh);
+            this.Controls.Add(this.btn_Detail);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Edit);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "UC_HoaDon";
+            this.Size = new System.Drawing.Size(1316, 702);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Button btn_Detail;
+        private Button btn_Delete;
+        private Button btn_Edit;
+
+        private TextBox tb_IdNhanVien;
+
+        private Label label5;
+        private TextBox tb_SoDienThoai;
+        private Panel panel2;
+        private Label label14;
+        private Label label15;
+        private Label label13;
+        private TextBox tb_KhachHang;
+        private DataGridView dataGridView1;
+        private Label label3;
+        private Button btn_Save;
+        private DateTimePicker dtp_NgayTao;
+        private Button btn_AddThongTinBaoHanh;
+    }
+}
---
+++ GUI/UC_ThongKe.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Do_An.BLL;
+namespace Do_An
+{
+    public partial class UC_ThongKe : UserControl
+    {
+        private static UC_ThongKe _instance;
+        public static UC_ThongKe Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new UC_ThongKe();
+                return _instance;
+            }
+        }
+        public UC_ThongKe()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_TK_Click(object sender, EventArgs e)
+        {
+            DateTime d1 = Convert.ToDateTime(datetime1.Value);
+            DateTime d2 = Convert.ToDateTime(datetime2.Value);
+         
+            if (d1<=d2)
+            {
+               // MessageBox.Show("đúng");
+
+                // số hàng đã nhập 1
+                  label1.Text = (BLL_ThongKe.Instance.SoSPDaNhap(d1, d2)).ToString();
+               
+                // số hàng đã bán được 2
+                label2.Text = (BLL_ThongKe.Instance.SoSPBanDuoc(d1, d2)).ToString();
+
+
+                // tổng chi 13
+               label13.Text = (BLL_ThongKe.Instance.TongChi(d1, d2)).ToString("N0") + " VND";
+                // tổng doanh thu 14
+                label14.Text = (BLL_ThongKe.Instance.TongDoanhThu(d1, d2)).ToString("N0") + " VND";
+                // lợi nhuận 7
+                label7.Text = (BLL_ThongKe.Instance.TongLoiNhuan(d1, d2)).ToString("N0") + " VND";
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn thời gian thống kê hợp lệ !!!! ");
+            }
+          
+        }
+    }
+}
